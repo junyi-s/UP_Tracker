efee6258be4bc6a38adbedb35a8fa4ef52323c31
Juana Ng <juanangnyc@gmail.com>
2021-11-03 01:12:58 -0400

    created desktop version of tracking info

--- front-end/src/components/trackingInfo/index.js
+++ front-end/src/components/trackingInfo/index.js
@@ -38,27 +38,58 @@ const TrackingInfo = () => {
       {/* Tracking Info */}
       <div className="rect">
         <div className="rectContent">
-          <p>
-            Package 1
-            <FaPen
-              style={{
-                position: "relative",
-                top: "2px",
-                left: "8px",
-                color: "#2B2C34",
-              }}
-            />
-          </p>
-          <p>USPS: 9400109205568018651239 </p>
-          <p>
-            Expected Delivery Date:{" "}
-            <span className="redColorBold">October 27</span>
-          </p>
-          <span className="circle"></span>
-          <div className="trackingHistoryPreview">
+          <div className="leftDesk">
+            <p>
+              Package 1
+              <FaPen
+                className="penIcon"
+                style={{
+                  position: "relative",
+                  top: "2px",
+                  left: "8px",
+                  color: "#2B2C34",
+                }}
+              />
+            </p>
+            <p className="mobile">USPS: 9400109205568018651239</p>
+            <p>
+              Expected Delivery Date:{" "}
+              <span className="redColorBold">October 27</span>
+            </p>
+          </div>
+          <div className="rightDesk">
+            <p className="desktop">Tracking Number: 9400109205568018651239</p>
+            <p className="desktop">Carrier: USPS</p>
+          </div>
+          <span className="circle mobile"></span>
+          <div className="trackingHistoryPreview mobile">
             <p>In Transit - DALLAS, TX</p>
             <p>Arrived at USPS Regional Facility</p>
           </div>
+
+          <div className="desktop shipmentHistory">
+            <ul className="progressTracker">
+              <li className="progressStep completed">
+                <span className="circle"></span>
+                <div className="label">
+                  <p>Origin - BROOKLYN, NY</p>
+                </div>
+              </li>
+              <li className="progressStep active">
+                <span className="circle"></span>
+                <div className="label">
+                  <p>In Transit - DALLAS, TX</p>
+                </div>
+              </li>
+              <li className="progressStep">
+                <span className="circle"></span>
+                <div className="label">
+                  <p></p>
+                </div>
+              </li>
+            </ul>
+          </div>
+
           <p className="detailsBtn">
             View Details
             <FaChevronDown
