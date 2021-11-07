36f936e59bf9da2fcde22c47113532cacedd5bf0
AlexanderBobb <ab7253@nyu.edu>
2021-11-07 11:16:16 -0800

    fixed form to save data

--- front-end/src/pages/home/index.js
+++ front-end/src/pages/home/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'
-import Header from '../../components/BootstrapNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SimpleForm from '../../components/SimpleForm';
 
 const Home = () => {
+    const [value, changevalue] = useState("");
+    const [name, changename] = useState("");
+    function handleChange(event) {
+        changevalue(event.target.value);
+    }
+
+    function change() {
+        changename(value);
+    }
     return (
         <div className="App">
             <header className="App-header">
-            <SimpleForm/>
+                <header className="form">
+                    <input
+                        onChange={handleChange}
+                        type="text"
+                        placeholder="Enter tracking number"
+                    />
+                </header>
+                <header classname="button">
+                    <button onClick={change}>Submit</button>
+                </header>
+                <p>
+                    Your tracking number is: {name}
+                </p>
             </header>
         </div>
     );
