542351b533ee0f134f2081454915207fc135f0bf
jw5933 <jw5933@nyu.edu>
2021-11-30 10:54:03 -0500

    added links to the footer
    + link to: github, confluence, about page

--- front-end/src/components/footer.js
+++ front-end/src/components/footer.js
@@ -1,9 +1,21 @@
 import React from "react"
 import './styles.css'
+import {Col, Row} from "react-bootstrap";
+import {BrowserRouter, Link} from 'react-router-dom';
 
 const Footer = () => (
   <div className="footer">
-    <p>Footer</p>
+    <Row>
+      <Col>
+        <a href="https://github.com/junyi-s/UP_Tracker" target="_blank" class = "footer-link">GitHub</a>
+      </Col>
+      <Col>
+        <a href="https://u-package-tracker.atlassian.net/l/c/ip6C681r" target="_blank" class = "footer-link">How to Use</a>
+      </Col>
+      <Col>
+        <a href="/about" class = "footer-link">Let's Chat!</a>
+      </Col>
+    </Row>
   </div>
 );
 
