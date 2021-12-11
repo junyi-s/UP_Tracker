5dbfea82b3fdc0f8d2734a7d50bc5ea0e66bbc50
Aila Aamir <ailaaamir10044@gmail.com>
2021-12-11 15:34:23 -0500

    adding png icons for footer

--- front-end/src/components/footer.js
+++ front-end/src/components/footer.js
@@ -2,18 +2,27 @@ import React from "react"
 import './styles.css'
 import {Col, Row} from "react-bootstrap";
 import {BrowserRouter, Link} from 'react-router-dom';
+import github from "./icons/github.png";
+import confluence from "./icons/confluence.png";
+import aboutus from "./icons/aboutus.png";
 
 const Footer = () => (
   <div className="footer">
     <Row>
       <Col>
-        <a href="https://github.com/junyi-s/UP_Tracker" target="_blank" class = "footer-link">GitHub</a>
+      <a href="https://github.com/junyi-s/UP_Tracker">
+        <img src={github} className="github" style={{height: "33px", left:"5%"}}/>
+      </a>
       </Col>
       <Col>
-        <a href="https://u-package-tracker.atlassian.net/l/c/ip6C681r" target="_blank" class = "footer-link">How to Use</a>
+      <a href="https://u-package-tracker.atlassian.net/wiki/spaces/PT/pages/393219/User+Guide">
+        <img src={confluence} className="confluence" style={{height: "40px"}}/>
+      </a>      
       </Col>
       <Col>
-        <a href="/about" class = "footer-link">Let's Chat!</a>
+      <a href="/about">
+        <img src={aboutus} className="about us" style={{height: "35px"}}/>
+      </a>        
       </Col>
     </Row>
   </div>
