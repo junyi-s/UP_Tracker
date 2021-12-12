d17107fe50af57cbdc037e4cb3f045b26c7f64bb
junyi-s <60148174+junyi-s@users.noreply.github.com>
2021-12-11 22:01:40 -0500

    changes

---
+++ front-end/src/components/data.js
@@ -0,0 +1,18 @@
+export const Data = [
+    {
+      question: 'How to Use                                ',
+      answer: '- A labracadabrador.'
+    },
+    {
+      question: 'Map Instructions',
+      answer: 'Hill-arious.'
+    },
+    {
+      question: 'Developer Notes',
+      answer: 'I Apollo-gize.'
+    },
+    {
+        question: 'Lorem ipsum',
+        answer: 'I Apollo-gize.'
+      }
+  ];
