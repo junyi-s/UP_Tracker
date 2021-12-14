595daaac2e76abd2ddbe53ed48df9f79c7dec5b5
junyi-s <60148174+junyi-s@users.noreply.github.com>
2021-12-13 20:13:30 -0500

    readme and footer changes

--- front-end/src/components/footer.js
+++ front-end/src/components/footer.js
@@ -1,21 +1,31 @@
 import React from "react"
 import './styles.css'
 import {Col, Row} from "react-bootstrap";
-
+import {BrowserRouter, Link} from 'react-router-dom';
+import github from "./icons/github.png";
+import confluence from "./icons/confluence.png";
+import aboutus from "./icons/aboutus.png";
+ 
 const Footer = () => (
-  <div className="footer">
-    <Row>
-      <Col>
-        <a href="https://github.com/junyi-s/UP_Tracker" target="_blank" class = "footer-link">GitHub</a>
-      </Col>
-      <Col>
-        <a href="https://u-package-tracker.atlassian.net/l/c/ip6C681r" target="_blank" class = "footer-link">Run A Local Site</a>
-      </Col>
-      <Col>
-        <a href="/about" class = "footer-link">Let's Chat!</a>
-      </Col>
-    </Row>
-  </div>
+ <div className="footer">
+   <Row>
+     <Col>
+     <a href="https://github.com/junyi-s/UP_Tracker">
+       <img src={github} className="github" style={{height: "33px", left:"5%"}}/>
+     </a>
+     </Col>
+     <Col>
+     <a href="https://u-package-tracker.atlassian.net/wiki/spaces/PT/pages/393219/User+Guide%22%3E">
+       <img src={confluence} className="confluence" style={{height: "40px"}}/>
+     </a>
+     </Col>
+     <Col>
+     <a href="/about">
+       <img src={aboutus} className="about us" style={{height: "35px"}}/>
+     </a>
+     </Col>
+   </Row>
+ </div>
 );
-
+ 
 export default Footer;
